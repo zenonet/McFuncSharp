49317f1e0ee46f7b26ebfce8da06f7d19c289379
zenonet <78077158+zenonet@users.noreply.github.com>
2022-11-26 15:48:52 +0100

    Created a constructor for Setblock.cs

--- FuncSharp/Commands/Setblock.cs
+++ FuncSharp/Commands/Setblock.cs
@@ -7,6 +7,12 @@ public class SetBlock : CommandBase
     public Vector Position;
     public Block Block;
 
+    public SetBlock(Vector position, Block block)
+    {
+        Position = position;
+        Block = block;
+    }
+
     public override string Generate()
     {
         return $"/setblock {Position} {Block}";
