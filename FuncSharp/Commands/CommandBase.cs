725d6a016229dc1f91f440897c44b1169d21a2f2
zenonet <78077158+zenonet@users.noreply.github.com>
2022-11-26 18:15:11 +0100

    Fixed some problems with run as in CommandBase.cs

--- FuncSharp/Commands/CommandBase.cs
+++ FuncSharp/Commands/CommandBase.cs
@@ -27,14 +27,14 @@ public abstract class CommandBase
             {
                 execute += $"run execute if entity {RunAs} as {RunAs} run {GenerateInternal()}\n" +
                            $"execute unless entity {RunAs} run summon marker ~ ~ ~ {{Tags:[\"funcsharp_run_at_helper\"], CustomName:'{{\"text\":\"{RunAs}\"}}'}}\n" +
-                           $"execute unless entity {RunAs} run execute as @e[type=marker,tag=funcsharp_run_at_helper, limit=1] at {RunAt} run {GenerateInternal()}\n" +
-                           $"execute unless entity RunAs run kill @e[type=marker,tag=funcsharp_run_at_helper, limit=1]";
+                           $"execute unless entity {RunAs} run execute as @e[type=marker,tag=funcsharp_run_at_helper, limit=1] {(RunAt == null ? string.Empty : $"at {RunAt} ")}run {GenerateInternal()}\n" +
+                           $"execute unless entity RunAs run kill @e[type=marker,tag=funcsharp_run_at_helper]";
             }
             else
             {
                 // If the string contains spaces, it's a random string and we need to create an entity with that name
                 execute += $"run summon marker ~ ~ ~ {{Tags:[\"funcsharp_run_at_helper\"], CustomName:'{{\"text\":\"{RunAs}\"}}'}}\n" +
-                           $"execute as @e[type=marker,tag=funcsharp_run_at_helper, limit=1] " + (RunAt == null ? string.Empty : "at {RunAt} ") + $"run {GenerateInternal()}\n" +
+                           $"execute as @e[type=marker,tag=funcsharp_run_at_helper, limit=1] {(RunAt == null ? string.Empty : $"at {RunAt} ")}run {GenerateInternal()}\n" +
                            $"kill @e[type=marker,tag=funcsharp_run_at_helper, limit=1]";
             }
         }
