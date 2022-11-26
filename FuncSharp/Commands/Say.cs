4c6954514fcf176289e88cc49433dafefd90ac8b
zenonet <78077158+zenonet@users.noreply.github.com>
2022-11-26 18:08:51 +0100

    Created the Say command

---
+++ FuncSharp/Commands/Say.cs
@@ -0,0 +1,16 @@
+﻿namespace FuncSharp.Commands;
+
+public class Say : CommandBase
+{
+    public Say(string text)
+    {
+        Text = text;
+    }
+
+    public string Text { get; set; }
+
+    protected override string GenerateInternal()
+    {
+        return $"say {Text}";
+    }
+}
