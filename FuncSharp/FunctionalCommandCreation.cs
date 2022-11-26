a341c55fded2847bc239ccfa6e177f3bd8fbcc45
zenonet <78077158+zenonet@users.noreply.github.com>
2022-11-26 18:09:19 +0100

    Created a class more be able to create commands more easily

---
+++ FuncSharp/FunctionalCommandCreation.cs
@@ -0,0 +1,33 @@
+﻿using FuncSharp.Commands;
+using FuncSharp.Core;
+using FuncSharp.DataPackGen;
+
+namespace FuncSharp;
+
+public static class FunctionalCommandCreation
+{
+    private static List<CommandBase> OnLoadCommands = new();
+
+    public static void Generate(string path, string name)
+    {
+        DataPackGenerator generator = new(path, name);
+        
+        generator.AddEntrypoint(new LoadEntrypoint("load", OnLoadCommands.ToArray()));
+        
+        generator.Generate();
+    }
+    
+    public static Say Say(string message)
+    {
+        Say cmd = new(message);
+        OnLoadCommands.Add(cmd);
+        return cmd;
+    }
+
+    public static SetBlock Setblock(Vector position, Block block)
+    {
+        SetBlock cmd = new(position, block);
+        OnLoadCommands.Add(cmd);
+        return cmd;
+    }
+}
