0bab6401131a646c6579c6d9ecbdfdd355864346
zenonet <78077158+zenonet@users.noreply.github.com>
2022-11-26 19:46:10 +0100

    Added SetBlockMode

---
+++ FuncSharp/Commands/SetBlock.cs
@@ -0,0 +1,29 @@
+﻿using FuncSharp.Core;
+
+namespace FuncSharp.Commands;
+
+public class SetBlock : CommandBase
+{
+    public Vector Position;
+    public Block Block;
+    public SetBlockMode Mode;
+
+    public SetBlock(Vector position, Block block, SetBlockMode mode = SetBlockMode.Replace)
+    {
+        Position = position;
+        Block = block;
+        Mode = mode;
+    }
+
+    protected override string GenerateInternal()
+    {
+        return $"setblock {Position} {Block} {Mode.ToString().ToLower()}";
+    }
+}
+
+public enum SetBlockMode
+{
+    Destroy,
+    Keep,
+    Replace,
+}
