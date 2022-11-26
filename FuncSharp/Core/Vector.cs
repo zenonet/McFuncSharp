1c9b964d8f1cb08a8bc2080e6b5d17fbc38adbbf
zenonet <78077158+zenonet@users.noreply.github.com>
2022-11-26 23:20:52 +0100

    Created a constructor for Vector.cs that takes doubles as params and casts them into floats

--- FuncSharp/Core/Vector.cs
+++ FuncSharp/Core/Vector.cs
@@ -13,8 +13,34 @@ public class Vector
         Z = z;
     }
 
+    public Vector(double x, double y, double z)
+    {
+        X = (float)x;
+        Y = (float)y;
+        Z = (float)z;
+    }
     public override string ToString()
     {
         return $"{X} {Y} {Z}";
+    }
+    
+    public static Vector operator +(Vector a, Vector b)
+    {
+        return new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
+    
+    public static Vector operator -(Vector a, Vector b)
+    {
+        return new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    }
+    
+    public static Vector operator *(Vector a, float b)
+    {
+        return new(a.X * b, a.Y * b, a.Z * b);
+    }
+    
+    public static Vector operator /(Vector a, float b)
+    {
+        return new(a.X / b, a.Y / b, a.Z / b);
     }
 }
