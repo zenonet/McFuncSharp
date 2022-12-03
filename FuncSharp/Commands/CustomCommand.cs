62148d88a2abc59b4393880db538bb2597646958
zenonet <78077158+zenonet@users.noreply.github.com>
2022-12-03 18:48:53 +0100

    Created the CustomCommand class

---
+++ FuncSharp/Commands/CustomCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace FuncSharp.Commands;
+
+public class CustomCommand : CommandBase
+{
+    public string Command;
+
+    public CustomCommand(string command)
+    {
+        Command = command;
+    }
+
+    protected override string GenerateInternal()
+    {
+        return Command.TrimStart('/');
+    }
+}
