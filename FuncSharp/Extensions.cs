8b990a4d1464e94d2f07b559739fa093ee5d4393
zenonet <78077158+zenonet@users.noreply.github.com>
2023-01-15 17:09:35 +0100

    Created a class with extension methods in FuncSharp.Commands

---
+++ FuncSharp/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using FuncSharp.Commands;
+
+namespace FuncSharp;
+
+public static class Extensions
+{
+    public static CustomCommand CreateCommand(this string command)
+    {
+        return new (command);
+    }
+    public static CustomCommand[] CreateCommandArray(this string commands)
+    {
+        return commands.Split('\n').Select(x => x.CreateCommand()).ToArray();
+    }
+}
