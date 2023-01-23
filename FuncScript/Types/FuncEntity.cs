dfb1e8a70c1b0babcc84768793eedc78df819c99
zenonet <78077158+zenonet@users.noreply.github.com>
2023-01-23 20:57:08 +0100

    Created the FuncEntity class

---
+++ FuncScript/Types/FuncEntity.cs
@@ -0,0 +1,51 @@
+﻿using FuncSharp.Core;
+using SlowLang.Engine;
+using SlowLang.Engine.Tokens;
+
+namespace FuncScript.Types;
+
+public class FuncEntity : FuncScriptValue
+{
+    public Entity Value { get; }
+    
+    public FuncEntity(Entity value)
+    {
+        Value = value;
+    }
+    
+    public static bool TryParse(TokenList list, out FuncEntity result)
+    {
+        if (list.Peek().Type != TokenType.Keyword || list.Peek().RawContent != "entity")
+        {
+            result = null;
+            return false;
+        }
+
+        list.Pop();
+
+        if (!list.StartsWith(TokenType.Colon))
+        {
+            result = null;
+            return false;
+        }
+
+        list.Pop();
+        
+        if (!Enum.TryParse(list.Peek().RawContent, out Entity entity))
+        {
+            LoggingManager.LogError("Unknown entity type: " + list.Peek().RawContent);
+            result = null;
+            return false;
+        }
+
+        list.Pop();
+        result = new (entity);
+
+        return true;
+    }
+
+    public override string Generate()
+    {
+        return $"minecraft:{Value}";
+    }
+}
