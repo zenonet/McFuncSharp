6a30c8ae431cf29ec87f91dd3f8c373b1acece7e
zenonet <78077158+zenonet@users.noreply.github.com>
2023-01-24 17:43:47 +0100

    Created FunsScriptValue.AsVarnameProvider()

--- FuncScript/Types/FuncScriptValue.cs
+++ FuncScript/Types/FuncScriptValue.cs
@@ -1,4 +1,5 @@
-﻿using SlowLang.Engine.Values;
+﻿using SlowLang.Engine;
+using SlowLang.Engine.Values;
 
 namespace FuncScript.Types;
 
@@ -7,5 +8,12 @@ public abstract class FuncScriptValue : Value
     public virtual string Generate()
     {
         return string.Empty;
+    }
+
+    public string AsVarnameProvider()
+    {
+        if(this is not VariableNameProvider)
+            LoggingManager.LogError($"Cannot use {this.GetType().Name} as a variable name provider");
+        return ((VariableNameProvider) this).VariableName;
     }
 }
