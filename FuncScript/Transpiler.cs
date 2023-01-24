4b9265acb2fb40a6e44045c2320d812a3079b100
zenonet <78077158+zenonet@users.noreply.github.com>
2023-01-24 17:46:09 +0100

    Added some initialization stuff that is added to every data pack

--- FuncScript/Transpiler.cs
+++ FuncScript/Transpiler.cs
@@ -8,13 +8,37 @@ namespace FuncScript;
 public static class Transpiler
 {
     public static StringBuilder McFunctionBuilder { get; private set; } = new();
+    
 
-    public static void Transpile(string funcScriptCode)
+    public static void Transpile(string funcScriptCode, Config? config = null)
     {
+        config ??= new();
+        
         McFunctionBuilder = new();
 
         TokenList tokens = Lexer.Lex(funcScriptCode);
-
+        
+        switch (config.ReloadBehavior)
+        {
+            case ReloadBehavior.KillOld:
+                // Make sure the entities won't drop any items
+                McFunctionBuilder.AppendLine("execute as @e[tag=funcscript_controlled] run data merge entity @s {DeathLootTable:\"minecraft:empty\"}");
+                // Kill the entities
+                McFunctionBuilder.AppendLine("kill @e[tag=funcscript_controlled]");
+                break;
+            case ReloadBehavior.DetachOld:
+                // Detach the entities
+                McFunctionBuilder.AppendLine("execute as @e[tag=funcscript_controlled] run tag @s remove funcscript_controlled");
+                break;
+            case ReloadBehavior.KeepOld:
+                break;
+        }
+        
+        Add("kill @e[tag=funcscript_controlled]");
+        Add("scoreboard objectives add funcscript_computation dummy");
+        Add("data remove storage funcscript_memory { }");
+        
+        
         Statement.ParseMultiple(ref tokens);
 
         Console.WriteLine("Done!");
