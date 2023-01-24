20a1ae127eaa23a968398adc51ec0fb46cd5d152
zenonet <78077158+zenonet@users.noreply.github.com>
2023-01-24 17:44:40 +0100

    Created config.cs

---
+++ FuncScript/Config.cs
@@ -0,0 +1,28 @@
+﻿namespace FuncScript;
+
+public class Config
+{
+    public ReloadBehavior ReloadBehavior { get; set; } = ReloadBehavior.DetachOld;
+}
+
+/// <summary>
+/// Specifies the behavior of the datapack when it is reloaded and there is stuff from the previous reload.
+/// </summary>
+public enum ReloadBehavior
+{
+    /// <summary>
+    /// Kills all entities from the previous load.
+    /// </summary>
+    KillOld,
+    
+    /// <summary>
+    /// Just keeps the old entities.
+    /// Warning: this causes id conflicts and therefore unwanted behavior.
+    /// </summary>
+    KeepOld,
+    
+    /// <summary>
+    /// Keeps the old entities but remove their ids
+    /// </summary>
+    DetachOld,
+}
