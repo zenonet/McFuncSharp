ff1b9ff057e237ebb23d88f69a39ea6bd57dcdca
zenonet <78077158+zenonet@users.noreply.github.com>
2023-01-28 18:40:59 +0100

    Created methods of comparisons in Computation.cs

--- FuncScript/Internal/Computation.cs
+++ FuncScript/Internal/Computation.cs
@@ -19,19 +19,69 @@ public static class Computation
         return Copy(addent1, output) +
                "\nscoreboard players operation " + output + " " + ComputationScoreboard + " += " + addent2 + " " + ComputationScoreboard;
     }
+
     public static string Subtract(string minuend, string subtractor, string output)
     {
         return Copy(minuend, output) +
                "\nscoreboard players operation " + output + " " + ComputationScoreboard + " -= " + subtractor + " " + ComputationScoreboard;
     }
+
     public static string Multiply(string factor1, string factor2, string output)
     {
         return Copy(factor1, output) +
                "\nscoreboard players operation " + output + " " + ComputationScoreboard + " *= " + factor2 + " " + ComputationScoreboard;
     }
+
     public static string Divide(string divident, string divisor, string output)
     {
         return Copy(divident, output) +
                "\nscoreboard players operation " + output + " " + ComputationScoreboard + " /= " + divisor + " " + ComputationScoreboard;
     }
+
+    public static string Modulo(string divident, string divisor, string output)
+    {
+        return Copy(divident, output) +
+               "\nscoreboard players operation " + output + " " + ComputationScoreboard + " %= " + divisor + " " + ComputationScoreboard;
+    }
+
+    #region Comparison
+
+    public static string Equal(string a, string b, string output)
+    {
+        return $"execute if score {a} {ComputationScoreboard} = {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
+               $"execute unless score {a} {ComputationScoreboard} = {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
+    }
+    
+    public static string NotEqual(string a, string b, string output)
+    {
+        return $"execute if score {a} {ComputationScoreboard} = {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0\n" +
+               $"execute unless score {a} {ComputationScoreboard} = {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1";
+    }
+    
+    public static string GreaterThan(string a, string b, string output)
+    {
+        return $"execute if score {a} {ComputationScoreboard} > {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
+               $"execute unless score {a} {ComputationScoreboard} > {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
+    }
+    
+    public static string GreaterThanOrEqual(string a, string b, string output)
+    {
+        return $"execute if score {a} {ComputationScoreboard} >= {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
+               $"execute unless score {a} {ComputationScoreboard} >= {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
+    }
+    
+    public static string LessThan(string a, string b, string output)
+    {
+        return $"execute if score {a} {ComputationScoreboard} < {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
+               $"execute unless score {a} {ComputationScoreboard} < {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
+    }
+    
+    public static string LessThanOrEqual(string a, string b, string output)
+    {
+        return $"execute if score {a} {ComputationScoreboard} <= {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
+               $"execute unless score {a} {ComputationScoreboard} <= {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
+    }
+
+
+    #endregion
 }
