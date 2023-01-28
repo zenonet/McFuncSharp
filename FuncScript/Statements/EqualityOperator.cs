27f6c81dc9ae60a089497ab9cb22bd471dfd1bea
zenonet <78077158+zenonet@users.noreply.github.com>
2023-01-28 18:41:38 +0100

    Implemented the equality operator

--- FuncScript/Statements/EqualityOperator.cs
+++ FuncScript/Statements/EqualityOperator.cs
@@ -1,31 +1,82 @@
-﻿using SlowLang.Engine;
+﻿using FuncScript.Internal;
+using FuncScript.Types;
+using SlowLang.Engine;
 using SlowLang.Engine.Initialization;
 using SlowLang.Engine.Statements;
 using SlowLang.Engine.Statements.StatementRegistrations;
 using SlowLang.Engine.Tokens;
+using SlowLang.Engine.Values;
 
 namespace FuncScript.Statements;
 
-public class EqualityOperator : Operator, IInitializable
+public class EqualityOperator : Statement, IInitializable
 {
+    
     public static void Initialize()
     {
-        StatementExtensionRegistration.CreateStatementExtensionRegistration<Statement, EqualityOperator>(TokenType.Equals, TokenType.Equals);
+        StatementRegistration.Create<EqualityOperator>(TokenType.OpeningBrace).Register();
     }
 
-    public override void OnParse(ref TokenList list, Statement baseStatement)
+    protected override bool CutTokensManually() => true;
+
+    private VariableNameProvider _variableNameProvider;
+
+    protected override bool OnParse(ref TokenList list)
     {
-        // Remove the 2 tokens that make up the operator
+        // Remove "op" keyword
+        //list.Pop();
+
+        // Remove opening brace
         list.Pop();
+
+        Statement left = Parse(ref list)!;
+
+        if (list.Peek().Type is not (TokenType.GreaterThan or TokenType.Equals or TokenType.LessThan))
+            return false;
+
+        if(list.Peek().Type == TokenType.Equals)
+        {
+            list.Pop();
+            
+            if (list.Peek().Type != TokenType.Equals)
+                LoggingManager.LogError("Expected second equals sign after first equals sign in equality operator");
+        }
+
+        TokenType operation = list.Pop().Type;
+        
+
+        Statement right = Parse(ref list)!;
+
+        // Move data to the computation scoreboard
+        MemoryManagement.MoveToComputationScoreboard(((VariableNameProvider) left.Execute()).VariableName, "a").Add();
+        MemoryManagement.MoveToComputationScoreboard(((VariableNameProvider) right.Execute()).VariableName, "b").Add();
+
+        // Execute operation and save the result in c on the computation scoreboard
+        (operation switch
+        {
+            TokenType.GreaterThan => Computation.GreaterThan("a", "b", "c"),
+            TokenType.LessThan => Computation.LessThan("a", "b", "c"),
+            TokenType.Equals => Computation.Equal("a", "b", "c"),
+        }).Add();
+
+        // Get a name for the result variable
+        string variableName = IdManager.GetId();
+
+        // Move result to the variable scoreboard
+        MemoryManagement.MoveToStorage(variableName, "c").Add();
+
+
+        // Create a variable name provider for the result
+        _variableNameProvider = new(variableName);
+
+        // Remove closing brace
         list.Pop();
-        
-        // Create a new statement for the right side of the operator
-        Statement? right = Parse(ref list);
-        
-        // If the right side is null, throw an exception
-        if (right == null)
-            throw new Exception("Expected expression after operator");
-        
-        
+
+        return true;
+    }
+
+    public override Value Execute()
+    {
+        return _variableNameProvider;
     }
 }
