915ac167188f5b69bbc8b426217415fb58f486f4
zenonet <78077158+zenonet@users.noreply.github.com>
2023-05-25 20:56:27 +0200

    Made raycastEntity() work

--- FuncScript/RaycastingResources.cs
+++ FuncScript/RaycastingResources.cs
@@ -1,4 +1,6 @@
-﻿using FuncScript.Internal;
+﻿using System.Globalization;
+using System.Text;
+using FuncScript.Internal;
 using FuncSharp;
 using FuncSharp.Core;
 
@@ -6,9 +8,15 @@ namespace FuncScript;
 
 public static class RaycastingResources
 {
-    public const string RaycastAccuracy = "0.1";
+    public const float RaycastAccuracy = 0.1f;
 
-    public const bool VisualizeRaycasts = false;
+    public static string StringRaycastAccuracy => RaycastAccuracy.ToString(CultureInfo.InvariantCulture);
+
+    public const float MaxRayLength = 50;
+
+    public const int IterationsToReachMaxRayLength = (int) (MaxRayLength / RaycastAccuracy);
+
+    public const bool VisualizeRaycasts = true;
 
     public static readonly BlockTag RaycastIgnoreBlockTag = new("funcscript_raycast_ignore", Block.water, Block.air, Block.cave_air, Block.void_air, Block.light, Block.structure_void, Block.tripwire, Block.tripwire_hook /*, Block.acacia_sign, Block.acacia_wall_sign, Block.birch_sign, Block.birch_wall_sign, Block.crimson_sign, Block.crimson_wall_sign*/);
 
@@ -22,49 +30,78 @@ public static class RaycastingResources
     {
         if (!Transpiler.AdditionalEntrypoints.Contains(RecursiveRaycastBlock))
             Transpiler.Generator.AddEntrypoint(RecursiveRaycastBlock);
-    }
-
-    public static void AddRaycastHitFunctions()
-    {
-        if (!Transpiler.AdditionalEntrypoints.Contains(RaycastHitBlock))
-            Transpiler.Generator.AddEntrypoint(RaycastHitBlock);
+        if (!Transpiler.AdditionalEntrypoints.Contains(RecursiveRaycastEntity))
+            Transpiler.Generator.AddEntrypoint(RecursiveRaycastEntity);
     }
 
     #region Recursive Raycasting Functions
 
-/*
-    public static string RecursiveRaycastAll =>
-        $"execute as @e[type=marker, tag=funcscript_ray] at @s run tp @s ^ ^ ^{RaycastAccuracy}\n" +
-        $"execute if entity @e[type=marker, tag=funcscript_ray, distance=..0.1] run function {Transpiler.Config.DataPackNameSpace}:raycast_hit_all\n" +
-        $"execute if block @e[type=marker, tag=funcscript_ray, distance=..0.1] run function {Transpiler.Config.DataPackNameSpace}:raycast_hit_all\n";
-
-    
-    public static string RecursiveRaycastEntity =>
-        $"execute as @e[type=marker, tag=funcscript_ray] at @s run tp @s ^ ^ ^{RaycastAccuracy}\n" +
-        $"execute if entity @e[type=marker, tag=funcscript_ray, distance=..0.1] run function {Transpiler.Config.DataPackNameSpace}:raycast_hit_entity\n";
-    */
-    public static FunctionEntrypoint RecursiveRaycastBlock => new ("recursive_raycast_block", (
+    public static string RecursiveRaycastAny =>
         // Move the ray forward
-        $"execute as @e[type=marker, tag=funcscript_ray] at @s run tp @s ^ ^ ^{RaycastAccuracy}\n" +
+        $"execute as @e[type=marker, tag=funcscript_ray] at @s run tp @s ^ ^ ^{StringRaycastAccuracy}\n" +
+        // If the raycast hit an entity, run the raycast hit function
+        $"execute if entity @e[type=marker, tag=funcscript_ray, distance=..0.1] run function {Transpiler.Config.DataPackNameSpace}:raycast_hit_any\n" +
+        // If the raycast hit a block, run the raycast hit function
+        $"execute if block @e[type=marker, tag=funcscript_ray, distance=..0.1] run function {Transpiler.Config.DataPackNameSpace}:raycast_hit_any\n" +
+        // Potentially create particles to visualize the ray
+        (VisualizeRaycasts ? "execute at @e[tag=funcscript_ray] run particle minecraft:enchanted_hit ~0.5 ~ ~0.5 0 0 0 0 10\n" : "") +
+        // Recursively call the function again
+        $"execute at @e[type=marker, tag=funcscript_ray] unless score iterations funcscript_computation > maxIterations funcscript_computation " +
+        $"if block ~ ~ ~ #minecraft:funcscript_raycast_ignore " +
+        $"run function {Transpiler.Config.DataPackNameSpace}:recursive_raycast_block\n";
+
+
+    public static FunctionEntrypoint RecursiveRaycastEntity
+    {
+        get
+        {
+            StringBuilder func = new();
+
+            // Increase the ray iteration couter
+            func.AppendLine("scoreboard players add iterations funcscript_computation 1");
+
+            // Check if the iteration counter is greater than the max ray iterations
+            func.AppendLine("");
+
+            // Move the ray forward
+            func.AppendLine($"execute as @e[type=marker, tag=funcscript_ray] at @s run tp @s ^ ^ ^{StringRaycastAccuracy}");
+
+            // If the raycast hit an entity, run save the position of the ray to variables.raycast_hit.hit_position
+            func.AppendLine(
+                $"execute at @e[tag=funcscript_ray] as @e[type=!marker, distance=..0.8] run " +
+                $"data modify storage {MemoryManagement.MemoryTag} variables.raycast_hit.hit_position set from entity @e[type=marker, tag=funcscript_ray, limit=1] Pos");
+
+            // If the raycast hit an entity, give that entity a tag
+            func.AppendLine(
+                $"execute at @e[tag=funcscript_ray] as @e[type=!marker, distance=..0.8] run " +
+                $"tag @s add funcscript_raycast_hit");
+
+            // Potentially create particles to visualize the ray
+            func.AppendLine(VisualizeRaycasts ? "execute at @e[tag=funcscript_ray] run particle minecraft:enchanted_hit ~0.5 ~ ~0.5 0 0 0 0 10\n" : ""); // Potentially create particles to visualize the ray
+
+            // Recursively call the function again
+            func.AppendLine(
+                $"execute at @e[tag=funcscript_ray] unless entity @e[type=!marker, distance=..0.8] " +
+                $"unless score iterations funcscript_computation > maxIterations funcscript_computation run " +
+                $"function {Transpiler.Config.DataPackNameSpace}:recursive_raycast_entity\n");
+
+            return new("recursive_raycast_entity", func.ToString().CreateCommandArray());
+        }
+    }
+
+
+    public static FunctionEntrypoint RecursiveRaycastBlock => new("recursive_raycast_block", (
+        // Move the ray forward
+        $"execute as @e[type=marker, tag=funcscript_ray] at @s run tp @s ^ ^ ^{StringRaycastAccuracy}\n" +
         // If the raycast hit a block, run the raycast hit block function
         $"execute at @e[type=marker, tag=funcscript_ray] unless block ~ ~ ~ #minecraft:funcscript_raycast_ignore " +
-        $"run function {Transpiler.Config.DataPackNameSpace}:raycast_hit_block\n" + 
-        // Potentially summon an entity to visualize the ray
+        $"run function {Transpiler.Config.DataPackNameSpace}:raycast_hit_block\n" +
+        // Potentially create particles to visualize the ray
         (VisualizeRaycasts ? "execute at @e[tag=funcscript_ray] run particle minecraft:enchanted_hit ~0.5 ~ ~0.5 0 0 0 0 10\n" : "") +
-        // Recusively call the function again
-        $"execute at @e[type=marker, tag=funcscript_ray] if block ~ ~ ~ #minecraft:funcscript_raycast_ignore " + 
+        // Recursively call the function again
+        $"execute at @e[type=marker, tag=funcscript_ray] unless score iterations funcscript_computation > maxIterations funcscript_computation " +
+        $"if block ~ ~ ~ #minecraft:funcscript_raycast_ignore " +
         $"run function {Transpiler.Config.DataPackNameSpace}:recursive_raycast_block\n"
-    ).CreateCommandArray());
-
-    #endregion
-
-    #region Callback Functions
-
-    public static FunctionEntrypoint RaycastHitBlock => new ("raycast_hit_block", (
-        // Save the hit position
-        $"data modify storage {MemoryManagement.MemoryTag} variables.raycast_hit.hit_position set from entity @e[type=marker, tag=funcscript_ray, limit=1] Pos\n" +
-        // Kill the ray
-        "kill @e[type=marker, tag=funcscript_ray]\n"
     ).CreateCommandArray());
 
     #endregion
--- FuncScript/Resources.cs
+++ FuncScript/Resources.cs
@@ -164,12 +164,10 @@ public static class Resources
 
                 RaycastingResources.AddRecursiveRaycastFunctions();
 
-                RaycastingResources.AddRaycastHitFunctions();
-
                 string id = IdManager.GetId();
 
                 string absoluteDirId = IdManager.GetId();
-                
+
                 ReturnValue = id;
 
                 Transpiler.MemoryTypes[id] = typeof(FuncVector);
@@ -186,7 +184,58 @@ public static class Resources
                        // Invoke the recursive function (start the raycasting process)
                        $"function {Transpiler.Config.DataPackNameSpace}:{RaycastingResources.RecursiveRaycastBlock.Name}\n" +
                        MemoryManagement.MoveVariable("raycast_hit.hit_position", id) + "\n" +
-                       "kill @e[tag=funcscript_raycastDirection, limit=1]";
+                       "kill @e[tag=funcscript_raycastDirection, limit=1]\n" +
+                       "kill @e[tag=funcscript_ray, limit=1]";
+            }
+        },
+        {
+            "raycastEntity", parameters =>
+            {
+                if (parameters.Length != 2)
+                    LoggingManager.LogError($"The raycast function takes two arguments but received {parameters.Length} arguments.");
+
+                if (!parameters[0].IsOfType<FuncVector>())
+                    LoggingManager.LogError("The raycast function takes a vector (the starting position) as its first argument but received " + parameters[0].GetFuncTypeName());
+
+                if (!parameters[0].IsOfType<FuncVector>())
+                    LoggingManager.LogError("The raycast function takes a vector (the ray direction) as its second argument but received " + parameters[1].GetFuncTypeName());
+
+                RaycastingResources.AddRaycastIgnoreBlockTag();
+
+                RaycastingResources.AddRecursiveRaycastFunctions();
+
+                string id = IdManager.GetId();
+
+                string absoluteDirId = IdManager.GetId();
+
+                ReturnValue = id;
+
+                Transpiler.MemoryTypes[id] = typeof(FuncEntity);
+
+                return
+                    // Prepare the iteration counter:
+                    $"scoreboard players reset iterations {Computation.ComputationScoreboard}\n" +
+                    $"scoreboard players set maxIterations {Computation.ComputationScoreboard} {RaycastingResources.IterationsToReachMaxRayLength}\n" +
+                    // Dereference the last raycast hit:
+                    $"tag @e[tag=funcscript_raycast_hit] remove funcscript_raycast_hit\n" +
+                    // Adding the raycast direction to the rays origin to get the position the ray should look at
+                    FuncVector.VectorAdd(parameters[0].AsVarnameProvider(), parameters[1].AsVarnameProvider(), absoluteDirId) + "\n" +
+                    // Summon the ray:
+                    "summon marker ~ ~ ~ {\"Tags\":[\"funcscript_controlled\", \"funcscript_ray\"]}\n" +
+                    $"data modify entity @e[tag=funcscript_ray, limit=1] Pos set from storage {MemoryManagement.MemoryTag} variables.{parameters[0].AsVarnameProvider()}\n" + // Set the ray start position
+                    // Summon the raycast direction marker:
+                    "summon marker ~ ~ ~ {\"Tags\":[\"funcscript_controlled\", \"funcscript_raycastDirection\"]}\n" +
+                    $"data modify entity @e[tag=funcscript_raycastDirection, limit=1] Pos set from storage {MemoryManagement.MemoryTag} variables.{absoluteDirId}\n" + // Set the raycast direction markers position
+                    // Make the ray look at the raycastDirection marker:
+                    $"execute as @e[tag=funcscript_ray] at @s run tp @s ~ ~ ~ facing entity @e[tag=funcscript_raycastDirection, limit=1] eyes\n" +
+                    // Invoke the recursive function (start the raycasting process)
+                    $"function {Transpiler.Config.DataPackNameSpace}:{RaycastingResources.RecursiveRaycastEntity.Name}\n" +
+                    // When the raycast is done, re-tag the entity with a new id
+                    "tag @e[tag=func_id_20] remove func_id_20\n" +
+                    $"tag @e[tag=funcscript_raycast_hit] add {id}\n" +
+                    $"tag @e[tag=funcscript_raycast_hit] remove funcscript_raycast_hit\n" +
+                    "kill @e[tag=funcscript_ray, limit=1]\n" +
+                    "kill @e[tag=funcscript_raycastDirection, limit=1]";
             }
         }
     };
