2a1a87cbc008a8903c3494b9f689002141d6d5bb
zenonet <78077158+zenonet@users.noreply.github.com>
2023-06-09 21:17:32 +0200

    Changed the names of func ids for easier debugging

--- FuncScript/Internal/IdManager.cs
+++ FuncScript/Internal/IdManager.cs
@@ -5,18 +5,18 @@ public static class IdManager
     private static int dataId;
     public static string GetDataId()
     {
-        return "func_data_id_" + (dataId++);
+        return "func_data_id_" + dataId++;
     }
     
     private static int entityId;
     public static string GetEntityId()
     {
-        return "func_entity_id_" + (entityId++);
+        return "func_entity_id_" + entityId++;
     }
     
     private static int functionId;
     public static string GetFunctionId()
     {
-        return "func_function_id_" + (functionId++);
+        return "func_function_id_" + functionId++;
     }
 }
