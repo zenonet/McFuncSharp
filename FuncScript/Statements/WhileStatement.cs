e31ef7050fd99ef2106cdb3de282ba392f97bce8
zenonet <78077158+zenonet@users.noreply.github.com>
2023-06-22 20:43:27 +0200

    Added WhileStatements

---
+++ FuncScript/Statements/WhileStatement.cs
@@ -0,0 +1,99 @@
+﻿using FuncScript.Internal;
+using FuncScript.Types;
+using FuncSharp;
+using SlowLang.Engine;
+using SlowLang.Engine.Initialization;
+using SlowLang.Engine.Statements;
+using SlowLang.Engine.Statements.StatementRegistrations;
+using SlowLang.Engine.Tokens;
+using SlowLang.Engine.Values;
+
+namespace FuncScript.Statements;
+
+public class WhileStatement : Statement, IInitializable
+{
+    public static void Initialize()
+    {
+        StatementRegistration.Create<WhileStatement>(list => list.Peek().RawContent == "while", TokenType.Keyword, TokenType.OpeningBrace).Register();
+    }
+
+    protected override bool CutTokensManually()
+    {
+        return true;
+    }
+
+    protected override bool OnParse(ref TokenList list)
+    {
+        // Remove the if keyword
+        list.Pop();
+
+        // Remove the opening brace
+        list.Pop();
+
+        // Parse the condition
+        Value? condition = null;
+
+        TokenList listCopy = list;
+        string conditionCode = Transpiler.YoinkGeneratedCode(() => condition = Statement.Parse(ref listCopy)!.Execute());
+        list = listCopy;
+
+        if (condition == null)
+        {
+            LoggingManager.LogError("Invalid condition in while statement");
+            return false;
+        }
+
+        string prefixForAllBodyStatements;
+        if (condition is ConstFuncScriptValue)
+        {
+            // TODO: Allow for const values as the condition of an if statement
+            throw new NotImplementedException("Const values as the condition of an if statement is not yet implemented");
+        }
+        else
+            prefixForAllBodyStatements = $"execute if data storage {MemoryManagement.MemoryTag} {{variables:{{{condition.AsVarnameProvider()}:1}}}} run ";
+
+
+        // Remove the closing brace
+        if (!list.StartsWith(TokenType.ClosingBrace))
+            LoggingManager.LogError("Expected closing brace after while statement condition");
+
+        list.Pop();
+
+        if (!list.StartsWith(TokenType.OpeningCurlyBrace))
+            LoggingManager.LogError("Expected opening curly brace after while statement condition");
+
+        list.Pop();
+
+        // Parse the body
+        TokenList? bodyTokenList = list.FindBetweenBraces(TokenType.OpeningCurlyBrace, TokenType.ClosingCurlyBrace, Logger);
+
+        if (bodyTokenList == null)
+            LoggingManager.LogError("Invalid body after while statement condition");
+
+        // Cut the body tokens from the list
+        list.RemoveRange(..bodyTokenList.List.Count);
+
+        // Remove the closing curly brace
+        list.Pop();
+
+        string loopCode = Transpiler.YoinkGeneratedCode(() => ParseMultiple(ref bodyTokenList));
+
+
+        string loopFunctionName = IdManager.GetFunctionId();
+
+        string conditionalLoopCall = $"{conditionCode}\n" +
+                                     $"execute if data storage {MemoryManagement.MemoryTag} {{variables:{{{condition.AsVarnameProvider()}:1b}}}} run function {Transpiler.Config.DataPackNameSpace}:{loopFunctionName}\n";
+
+        // Add a recursive call to the loop function
+        loopCode += conditionalLoopCall;
+
+        // Create a loop entrypoint
+        FunctionEntrypoint loopFunction = new(loopFunctionName, loopCode.CreateCommandArray());
+        Transpiler.AdditionalEntrypoints.Add(loopFunction);
+
+        // Make the loop run recursively
+        Transpiler.McFunctionBuilder.Append(conditionalLoopCall);
+
+        return true;
+    }
+}
