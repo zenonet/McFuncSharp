5370e22e0920f83776a227c4a89be566a4c7eaf3
zenonet <78077158+zenonet@users.noreply.github.com>
2023-09-21 21:48:28 +0200

    Added ParenthesesStatement.cs which wraps another statement to isolate it from operator parsing

---
+++ FuncScript/Statements/ParenthesesStatement.cs
@@ -0,0 +1,50 @@
+﻿using FuncScript.Types;
+using SlowLang.Engine;
+using SlowLang.Engine.Initialization;
+using SlowLang.Engine.Statements;
+using SlowLang.Engine.Statements.StatementRegistrations;
+using SlowLang.Engine.Tokens;
+using SlowLang.Engine.Values;
+
+namespace FuncScript.Statements;
+
+public class ParenthesesStatement : Statement, IInitializable
+{
+    private VariableNameProvider variableNameProvider = null!;
+
+    public static void Initialize()
+    {
+        StatementRegistration.Create<ParenthesesStatement>(list => list.Peek().Type == TokenType.OpeningBrace).Register();
+    }
+
+    protected override bool CutTokensManually() => true;
+
+    public override bool OnParse(ref TokenList list)
+    {
+        list.Pop();
+        TokenList? innerList = list.FindBetweenBraces(TokenType.OpeningBrace, TokenType.ClosingBrace, Logger);
+        if (innerList == null)
+            return false;
+
+        list.RemoveRange(..innerList.List.Count);
+
+        Statement statement = Parse(ref innerList);
+        if (statement == null)
+            return false;
+
+        variableNameProvider = (VariableNameProvider) statement.Execute();
+
+        
+        if(!list.StartsWith(TokenType.ClosingBrace))
+            LoggingManager.LogError("Expected closing brace after statement in parentheses");
+        
+        list.Pop();
+        
+        return true;
+    }
+
+    public override Value Execute()
+    {
+        return variableNameProvider;
+    }
+}
