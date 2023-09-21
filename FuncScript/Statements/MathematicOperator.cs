1d26b45dc74ac2ef2ebf9026aa3bf91f15d8f8ba
zenonet <78077158+zenonet@users.noreply.github.com>
2023-09-21 21:35:52 +0200

    Completely reworked MathematicOperators. They now work like in any other programming language

--- FuncScript/Statements/MathematicOperator.cs
+++ FuncScript/Statements/MathematicOperator.cs
@@ -1,4 +1,6 @@
-﻿using FuncScript.Internal;
+﻿using System;
+using System.Collections.Generic;
+using FuncScript.Internal;
 using FuncScript.Types;
 using SlowLang.Engine;
 using SlowLang.Engine.Initialization;
@@ -9,103 +11,112 @@ using SlowLang.Engine.Values;
 
 namespace FuncScript.Statements;
 
-public class MathematicOperator : Statement, IInitializable
+public class MathematicOperator : StatementExtension, IInitializable
 {
     public static void Initialize()
     {
-        StatementRegistration.Create<MathematicOperator>(TokenType.OpeningBrace).Register();
+        StatementExtensionRegistration.CreateStatementExtensionRegistration<Statement, MathematicOperator>(
+            list => list.List.Count != 0 && list.Peek().Type.IsMathematicOperator()
+        ).Register();
     }
 
     protected override bool CutTokensManually() => true;
 
-    private VariableNameProvider _variableNameProvider;
+    private VariableNameProvider variableNameProvider;
 
-    protected override bool OnParse(ref TokenList list)
+    private static readonly HashSet<Statement> ChildStatements = new();
+    private TokenType Operation { get; set; }
+
+    private Statement baseStatement = null!;
+    private Statement rightSide = null!;
+
+    public override bool OnParse(ref TokenList list, Statement baseStatement)
     {
-        // Remove "op" keyword
-        //list.Pop();
+        this.baseStatement = baseStatement;
+        Operation = list.Pop().Type;
 
-        // Remove opening brace
-        list.Pop();
-
-        Statement left = Parse(ref list)!;
-
-        if (list.Peek().Type is not (TokenType.Plus or TokenType.Minus or TokenType.Multiply or TokenType.Divide))
+        Statement? rightSideOrNull = Statement.Parse(ref list, onStatementInstantiated: statement => { ChildStatements.Add(statement); });
+        if (rightSideOrNull == null)
             return false;
 
-        // Remove operator
-        TokenType operation = list.Pop().Type;
+        rightSide = rightSideOrNull;
 
-        Statement right = Parse(ref list)!;
+        ChildStatements.Remove(rightSide);
 
-        FuncScriptValue leftValue = (FuncScriptValue) left.Execute();
-        FuncScriptValue rightValue = (FuncScriptValue) right.Execute();
+        // If this is not the root operator, we don't need to do anything
+        if (ChildStatements.Contains(baseStatement))
+            return true;
+        
+        // If this is the root operator, we need to parse the whole expression:
 
-        // Get a name for the result variable
-        string variableName = IdManager.GetDataId();
-        // Create a variable name provider for the result
-        _variableNameProvider = new(variableName);
+        // Get all statements and operations that are part of this expression
+        ExtractExpression(baseStatement, out List<Statement> statements, out List<TokenType> operations);
 
-        if (leftValue.IsOfType<FuncNumber>() && rightValue.IsOfType<FuncNumber>())
+        // Merge all operations by priority
+        while (statements.Count > 1)
         {
-            // Move data to the computation scoreboard
-            MemoryManagement.MoveToComputationScoreboard(((VariableNameProvider) leftValue).VariableName, "a").Add();
-            MemoryManagement.MoveToComputationScoreboard(((VariableNameProvider) rightValue).VariableName, "b").Add();
-
-            // Execute operation and save the result in c on the computation scoreboard
-            (operation switch
-            {
-                TokenType.Plus => Computation.Add("a", "b", "c"),
-                TokenType.Minus => Computation.Subtract("a", "b", "c"),
-                TokenType.Multiply => Computation.Multiply("a", "b", "c"),
-                TokenType.Divide => Computation.Divide("a", "b", "c"),
-            }).Add();
-
-            // Move result to the variable scoreboard
-            MemoryManagement.MoveToStorage(variableName, "c").Add();
-            Transpiler.MemoryTypes.Add(variableName, typeof(FuncNumber));
-        }
-        else if (leftValue.IsOfType<FuncVector>() && rightValue.IsOfType<FuncVector>())
-        {
-            // Execute operation and save the result
-            (operation switch
-            {
-                TokenType.Plus => FuncVector.VectorAdd(leftValue.AsVarnameProvider(), rightValue.AsVarnameProvider(), variableName),
-                TokenType.Minus => FuncVector.VectorSubtract(leftValue.AsVarnameProvider(), rightValue.AsVarnameProvider(), variableName),
-                TokenType.Multiply => FuncVector.VectorMultiply(leftValue.AsVarnameProvider(), rightValue.AsVarnameProvider(), variableName),
-                TokenType.Divide => FuncVector.VectorDivide(leftValue.AsVarnameProvider(), rightValue.AsVarnameProvider(), variableName),
-            }).Add();
-
-            Transpiler.MemoryTypes.Add(variableName, typeof(FuncVector));
-        }
-        else if (leftValue.IsOfType<FuncVector>() && rightValue.IsOfType<FuncNumber>() || leftValue.IsOfType<FuncNumber>() && rightValue.IsOfType<FuncVector>())
-        {
-            FuncScriptValue number = leftValue.IsOfType<FuncNumber>() ? leftValue : rightValue;
-            FuncScriptValue vector = leftValue.IsOfType<FuncVector>() ? leftValue : rightValue;
-
-            Transpiler.MemoryTypes.Add(variableName, typeof(FuncVector));
-
-            if (operation is TokenType.Multiply)
-            {
-                MulitplyVectorWithNumber(vector.AsVarnameProvider(), number.AsVarnameProvider(), variableName).Add();
-            }
-            else
-            {
-                LoggingManager.LogError($"Unable to apply the operation {operation} to a vector and a number");
-            }
-        }
-        else
-        {
-            LoggingManager.LogError($"Unable to apply the {operation} operator to {leftValue.GetFuncTypeName()} and {rightValue.GetFuncTypeName()}");
+            MergeOperation(operations, statements);
         }
 
-        // Remove closing brace
-        list.Pop();
+        Statement expression = statements[0];
+        variableNameProvider = (VariableNameProvider) expression.Execute();
 
         return true;
     }
 
-    private string MulitplyVectorWithNumber(string vector, string number, string variableName)
+    private void MergeOperation(List<TokenType> operations, List<Statement> statements)
+    {
+        int highestPriority = 0;
+        int highestPriorityOperationIndex = -1;
+        for (int i = operations.Count - 1; i >= 0; i--)
+        {
+            int priority = GetOperatorPriority(operations[i]);
+
+            // The priority does not need to be higher than max, because we are parsing in reverse meaning that the last operator is the first one to be executed
+            if (priority >= highestPriority)
+            {
+                highestPriority = priority;
+                highestPriorityOperationIndex = i;
+            }
+        }
+
+        // Combine the highest priority operation with the statements around it
+        ExpressionCompound compound = new(statements[highestPriorityOperationIndex], statements[highestPriorityOperationIndex + 1], operations[highestPriorityOperationIndex]);
+        statements.RemoveAt(highestPriorityOperationIndex);
+        statements.RemoveAt(highestPriorityOperationIndex);
+        operations.RemoveAt(highestPriorityOperationIndex);
+        statements.Insert(highestPriorityOperationIndex, compound);
+    }
+
+    private void ExtractExpression(Statement leftmostSide, out List<Statement> statements, out List<TokenType> operations)
+    {
+        Statement r = rightSide;
+        statements = new();
+        operations = new();
+        statements.Add(leftmostSide);
+        operations.Add(Operation);
+        while (r is MathematicOperator mathematicOperator)
+        {
+            operations.Add(mathematicOperator.Operation);
+            statements.Add(mathematicOperator.baseStatement);
+
+            r = mathematicOperator.rightSide;
+        }
+
+        statements.Add(r);
+    }
+
+    private int GetOperatorPriority(TokenType operation)
+    {
+        return operation switch
+        {
+            TokenType.Multiply or TokenType.Divide => 2,
+            TokenType.Plus or TokenType.Minus => 1,
+            _ => throw new ArgumentException("Invalid operation"),
+        };
+    }
+
+    private static string MulitplyVectorWithNumber(string vector, string number, string variableName)
     {
         return
             // Create a vector with the number in all components
@@ -117,8 +128,93 @@ public class MathematicOperator : Statement, IInitializable
             FuncVector.VectorMultiply(vector, "vectorized_number", variableName);
     }
 
+    private class ExpressionCompound : Statement
+    {
+        public readonly Statement LeftSide;
+        public readonly TokenType Operation;
+        public readonly Statement ReftSide;
+        private readonly string variableName;
+
+        public ExpressionCompound(Statement leftSide, Statement reftSide, TokenType operation)
+        {
+            LeftSide = leftSide;
+            ReftSide = reftSide;
+            Operation = operation;
+            variableName = IdManager.GetDataId();
+        }
+
+        public override string ToString() => $"({LeftSide} {Operation} {ReftSide})";
+
+        public override Value Execute()
+        {
+            // This is an exception to the idea, that Statements should add their generated mcfunction code 
+            
+            FuncScriptValue leftValue = (FuncScriptValue) LeftSide.Execute();
+            FuncScriptValue rightValue = (FuncScriptValue) ReftSide.Execute();
+            if (leftValue.IsOfType<FuncNumber>() && rightValue.IsOfType<FuncNumber>())
+            {
+                // Move data to the computation scoreboard
+                MemoryManagement.MoveToComputationScoreboard(((VariableNameProvider) leftValue).VariableName, "a").Add();
+                MemoryManagement.MoveToComputationScoreboard(((VariableNameProvider) rightValue).VariableName, "b").Add();
+
+                // Execute operation and save the result in c on the computation scoreboard
+                (Operation switch
+                {
+                    TokenType.Plus => Computation.Add("a", "b", "c"),
+                    TokenType.Minus => Computation.Subtract("a", "b", "c"),
+                    TokenType.Multiply => Computation.Multiply("a", "b", "c"),
+                    TokenType.Divide => Computation.Divide("a", "b", "c"),
+                }).Add();
+
+                // Move result to the variable scoreboard
+                MemoryManagement.MoveToStorage(variableName, "c").Add();
+                Transpiler.MemoryTypes.Add(variableName, typeof(FuncNumber));
+            }
+            else if (leftValue.IsOfType<FuncVector>() && rightValue.IsOfType<FuncVector>())
+            {
+                // Execute operation and save the result
+                (Operation switch
+                {
+                    TokenType.Plus => FuncVector.VectorAdd(leftValue.AsVarnameProvider(), rightValue.AsVarnameProvider(), variableName),
+                    TokenType.Minus => FuncVector.VectorSubtract(leftValue.AsVarnameProvider(), rightValue.AsVarnameProvider(), variableName),
+                    TokenType.Multiply => FuncVector.VectorMultiply(leftValue.AsVarnameProvider(), rightValue.AsVarnameProvider(), variableName),
+                    TokenType.Divide => FuncVector.VectorDivide(leftValue.AsVarnameProvider(), rightValue.AsVarnameProvider(), variableName),
+                }).Add();
+
+                Transpiler.MemoryTypes.Add(variableName, typeof(FuncVector));
+            }
+            else if (leftValue.IsOfType<FuncVector>() && rightValue.IsOfType<FuncNumber>() || leftValue.IsOfType<FuncNumber>() && rightValue.IsOfType<FuncVector>())
+            {
+                FuncScriptValue number = leftValue.IsOfType<FuncNumber>() ? leftValue : rightValue;
+                FuncScriptValue vector = leftValue.IsOfType<FuncVector>() ? leftValue : rightValue;
+
+                Transpiler.MemoryTypes.Add(variableName, typeof(FuncVector));
+
+                if (Operation is TokenType.Multiply)
+                {
+                    MulitplyVectorWithNumber(vector.AsVarnameProvider(), number.AsVarnameProvider(), variableName).Add();
+                }
+                else
+                {
+                    LoggingManager.LogError($"Unable to apply the operation {Operation} to a vector and a number");
+                }
+            }
+            else
+            {
+                LoggingManager.LogError($"Unable to apply the {Operation} operator to {leftValue.GetFuncTypeName()} and {rightValue.GetFuncTypeName()}");
+            }
+
+            return new VariableNameProvider(variableName);
+        }
+    }
+
     public override Value Execute()
     {
-        return _variableNameProvider;
+        return variableNameProvider;
+    }
+
+    public override string ToString()
+    {
+        return Operation.ToString();
     }
 }
