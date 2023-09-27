44e890dd701aabe17160521acd82bb406baed929
zenonet <78077158+zenonet@users.noreply.github.com>
2023-09-27 16:29:27 +0200

    Made all methods from MemoryManagement.cs and Computation.cs Pure, that only return a string that has to be added to the string builder

--- FuncScript/Internal/Computation.cs
+++ FuncScript/Internal/Computation.cs
@@ -1,4 +1,6 @@
-﻿namespace FuncScript.Internal;
+﻿using System.Diagnostics.Contracts;
+
+namespace FuncScript.Internal;
 
 public static class Computation
 {
@@ -9,35 +11,41 @@ public static class Computation
         return "scoreboard objectives add " + ComputationScoreboard + " dummy";
     }
 
+    [Pure]
     public static string Copy(string source, string target)
     {
         return "scoreboard players operation " + target + " " + ComputationScoreboard + " = " + source + " " + ComputationScoreboard;
     }
 
+    [Pure]
     public static string Add(string addent1, string addent2, string output)
     {
         return Copy(addent1, output) +
                "\nscoreboard players operation " + output + " " + ComputationScoreboard + " += " + addent2 + " " + ComputationScoreboard;
     }
 
+    [Pure]
     public static string Subtract(string minuend, string subtractor, string output)
     {
         return Copy(minuend, output) +
                "\nscoreboard players operation " + output + " " + ComputationScoreboard + " -= " + subtractor + " " + ComputationScoreboard;
     }
 
+    [Pure]
     public static string Multiply(string factor1, string factor2, string output)
     {
         return Copy(factor1, output) +
                "\nscoreboard players operation " + output + " " + ComputationScoreboard + " *= " + factor2 + " " + ComputationScoreboard;
     }
 
+    [Pure]
     public static string Divide(string divident, string divisor, string output)
     {
         return Copy(divident, output) +
                "\nscoreboard players operation " + output + " " + ComputationScoreboard + " /= " + divisor + " " + ComputationScoreboard;
     }
 
+    [Pure]
     public static string Modulo(string divident, string divisor, string output)
     {
         return Copy(divident, output) +
@@ -46,42 +54,47 @@ public static class Computation
 
     #region Comparison
 
+    [Pure]
     public static string Equal(string a, string b, string output)
     {
         return $"execute if score {a} {ComputationScoreboard} = {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
                $"execute unless score {a} {ComputationScoreboard} = {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
     }
-    
+
+    [Pure]
     public static string NotEqual(string a, string b, string output)
     {
         return $"execute if score {a} {ComputationScoreboard} = {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0\n" +
                $"execute unless score {a} {ComputationScoreboard} = {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1";
     }
-    
+
+    [Pure]
     public static string GreaterThan(string a, string b, string output)
     {
         return $"execute if score {a} {ComputationScoreboard} > {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
                $"execute unless score {a} {ComputationScoreboard} > {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
     }
-    
+
+    [Pure]
     public static string GreaterThanOrEqual(string a, string b, string output)
     {
         return $"execute if score {a} {ComputationScoreboard} >= {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
                $"execute unless score {a} {ComputationScoreboard} >= {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
     }
-    
+
+    [Pure]
     public static string LessThan(string a, string b, string output)
     {
         return $"execute if score {a} {ComputationScoreboard} < {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
                $"execute unless score {a} {ComputationScoreboard} < {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
     }
-    
+
+    [Pure]
     public static string LessThanOrEqual(string a, string b, string output)
     {
         return $"execute if score {a} {ComputationScoreboard} <= {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 1\n" +
                $"execute unless score {a} {ComputationScoreboard} <= {b} {ComputationScoreboard} run scoreboard players set {output} {ComputationScoreboard} 0";
     }
-
 
     #endregion
 }
--- FuncScript/Internal/MemoryManagement.cs
+++ FuncScript/Internal/MemoryManagement.cs
@@ -1,34 +1,39 @@
-﻿namespace FuncScript.Internal;
+﻿using System.Diagnostics.Contracts;
+
+namespace FuncScript.Internal;
 
 public static class MemoryManagement
 {
     public const string MemoryTag = "funcscript_memory";
 
+    [Pure]
     public static string GetVariable(string variableName)
     {
         return "data get storage " + MemoryTag + " variables." + variableName;
     }
 
+    [Pure]
     public static string SetVariable(string variableName, string value)
     {
         return "data modify storage " + MemoryTag + " variables." + variableName + " set value " + value;
     }
-    
+    [Pure]
     public static string MoveVariable(string variableName, string target)
     {
         return "data modify storage " + MemoryTag + " variables." + target + " set from storage " + MemoryTag + " variables." + variableName;
     }
 
+    [Pure]
     public static string FreeMemory(string variableName)
     {
         return "data remove storage " + MemoryTag + " variables." + variableName;
     }
-
+    [Pure]
     public static string MoveToComputationScoreboard(string variableName, string nameOnScoreboard)
     {
         return "execute store result score " + nameOnScoreboard + " " + Computation.ComputationScoreboard + " run data get storage " + MemoryTag + " variables." + variableName;
     }
-
+    [Pure]
     public static string MoveToStorage(string variableName, string nameOnScoreboard, string dataType = "double", float scale = 1)
     {
         return "execute store result storage " + MemoryTag + " variables." + variableName + $" {dataType} {scale.ToString("0." + new string('#', 339)).Replace(',', '.')} run scoreboard players get " + nameOnScoreboard + " " + Computation.ComputationScoreboard;
