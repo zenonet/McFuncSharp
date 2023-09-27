a8f330b38feed18e91500cf1edcb526af96e84d0
zenonet <78077158+zenonet@users.noreply.github.com>
2023-09-27 16:30:06 +0200

    Created the BreakStatement

---
+++ FuncScript/Statements/BreakStatement.cs
@@ -0,0 +1,24 @@
+﻿using FuncScript.Internal;
+using SlowLang.Engine.Initialization;
+using SlowLang.Engine.Statements;
+using SlowLang.Engine.Statements.StatementRegistrations;
+using SlowLang.Engine.Tokens;
+
+namespace FuncScript.Statements;
+
+public class BreakStatement : Statement, IInitializable
+{
+    public static void Initialize()
+    {
+        StatementRegistration.Create<BreakStatement>(list => list.Peek().RawContent == "break", TokenType.Keyword).Register();
+    }
+
+    public override bool OnParse(ref TokenList list)
+    {
+        Loop last = (Loop) Transpiler.StackTrace.Last(x => x is Loop);
+
+        MemoryManagement.SetVariable("break_from_" + last.LoopFunctionName, "1b").Add();
+        
+        return true;
+    }
+}
