e547728f90547db1d2fb42cb1795c6ac830a6ab9
zenonet <78077158+zenonet@users.noreply.github.com>
2023-09-28 17:11:32 +0200

    Added Utils.PathToIfData()

--- FuncScript/Utils.cs
+++ FuncScript/Utils.cs
@@ -11,4 +11,24 @@ public static class Utils{
     public static bool IsOperator(this TokenType t) => t is TokenType.Plus or TokenType.Minus or TokenType.Multiply or TokenType.Divide or TokenType.DoubleEquals or TokenType.GreaterThan or TokenType.LessThan;
     public static bool IsMathematicOperator(this TokenType t) => t is TokenType.Plus or TokenType.Minus or TokenType.Multiply or TokenType.Divide;
     public static bool IsEqualityOperator(this TokenType t) => t is TokenType.DoubleEquals or TokenType.GreaterThan or TokenType.LessThan;
+    
+    public static string PathToIfData(string path, string expectedValue)
+    {
+        // example input: "variables.ccu.i"
+        // example output: "{variables:{ccu:{i:1b}}}"
+        string[] parts = path.Split('.');
+        string output = "{";
+        for (int i = 0; i < parts.Length - 1; i++)
+        {
+            output += parts[i] + ":{"; // variables:{
+        }
+        output += parts[^1] + ":" + expectedValue;
+        for (int i = 0; i < parts.Length; i++)
+        {
+            output += "}";
+        }
+
+        return output;
+        
+    }
 }
