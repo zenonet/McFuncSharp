7bcb2e1f4d7f7fce2803f95586950f2eec686a57
zenonet <78077158+zenonet@users.noreply.github.com>
2023-10-01 12:32:59 +0200

    Removed out-commented code from Program.cs

--- FuncScript/Program.cs
+++ FuncScript/Program.cs
@@ -1,29 +1,6 @@
 ﻿using System.Diagnostics;
 using FuncScript;
 
-
-
-/*
-Computation.Initialize();
-
-StreamWriter writer = File.CreateText(@"C:\Users\zeno\MultiMC\instances\Funcsharp\.minecraft\saves\FuncSharp LOOOOL\datapacks\first_funcsharp\data\first_funcsharp\functions\load.mcfunction");
-
-writer.WriteLine(Computation.Initialize());
-
-writer.WriteLine(MemoryManagement.SetVariable("amogus", "6"));
-writer.WriteLine(MemoryManagement.SetVariable("abobus", "2"));
-
-writer.WriteLine(MemoryManagement.MoveToComputationScoreboard("amogus", "amogus"));
-writer.WriteLine(MemoryManagement.MoveToComputationScoreboard("abobus", "abobus"));
-
-writer.WriteLine(Computation.Divide("amogus", "abobus", "asbestos"));
-
-writer.WriteLine(MemoryManagement.MoveToStorage("asbestos", "asbestos"));
-
-writer.Close();
-*/
-
-
 if(args.Length != 2)
 {
     Console.WriteLine("Usage: FuncScript.exe <filename> <pathToDatapackDirectory>");
@@ -49,12 +26,3 @@ sw.Stop();
 
 Console.WriteLine($"Successfully transpiled in {sw.ElapsedMilliseconds}ms");
 return 0;
-
-/*LoadEntrypoint loadEntrypoint = new("load", Transpiler.McFunctionBuilder.ToString().CreateCommandArray());
-
-DataPackGenerator generator = new(@"C:\Users\zeno\MultiMC\instances\Funcsharp\.minecraft\saves\FuncSharp LOOOOL\datapacks\first_funcsharp\", "first_funcsharp");
-
-generator.AddEntrypoint(loadEntrypoint);
-
-generator.Generate();*/
-//File.WriteAllText(args[0], Transpiler.McFunctionBuilder.ToString());
