cf592c66ad3dc0fa33b543dc6d37fd5b176e909e
zenonet <78077158+zenonet@users.noreply.github.com>
2023-12-27 21:09:22 +0100

    Fixed increment/decrement statements not returning the value of the variable

--- FuncScript/Statements/IncrementStatement.cs
+++ FuncScript/Statements/IncrementStatement.cs
@@ -1,9 +1,11 @@
 ﻿using FuncScript.Internal;
+using FuncScript.Types;
 using SlowLang.Engine;
 using SlowLang.Engine.Initialization;
 using SlowLang.Engine.Statements;
 using SlowLang.Engine.Statements.StatementRegistrations;
 using SlowLang.Engine.Tokens;
+using SlowLang.Engine.Values;
 
 namespace FuncScript.Statements;
 
@@ -24,17 +26,20 @@ public class IncrementStatement : StatementExtension, IInitializable
         return true;
     }
 
+    private VariableNameProvider variableNameProvider;
+
     private Operator? Operator;
+
     public override bool OnParse(ref TokenList list, Statement baseStatement)
     {
         VariableCall baseVariableCall = (VariableCall) baseStatement;
 
         TokenType operation = list.Pop().Type;
         list.Pop();
-        
+
         // TODO: Fix
         MemoryManagement.MoveToComputationScoreboard(baseVariableCall.VariableName, "a").Add();
-        
+
         if (operation == TokenType.Plus)
         {
             Computation.Add("a", "one", "a").Add();
@@ -43,9 +48,14 @@ public class IncrementStatement : StatementExtension, IInitializable
         {
             Computation.Subtract("a", "one", "a").Add();
         }
-        
-        MemoryManagement.MoveToStorage(baseVariableCall.VariableName, "a").Add();
 
+        MemoryManagement.MoveToStorage(baseVariableCall.VariableName, "a").Add();
+        variableNameProvider = new(baseVariableCall.VariableName);
         return true;
+    }
+
+    public override Value Execute()
+    {
+        return variableNameProvider;
     }
 }
