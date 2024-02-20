f03b4649ac9b20352577dfd4626e3d177904ca0c
zenonet <78077158+zenonet@users.noreply.github.com>
2024-02-20 21:44:17 +0100

    Removed unused method Escape()

--- FuncScript/Utils.cs
+++ FuncScript/Utils.cs
@@ -1,13 +1,8 @@
-using System.Text.RegularExpressions;
+using FuncScript.Types;
 using SlowLang.Engine.Tokens;
 
 public static class Utils{
     
-    private static string Escape(string input)
-    {
-        return new Regex("[\\[\\\\\\^\\$\\.\\|\\?\\*\\+\\(\\)\\{\\}\\-\\%]").Replace(input, "\\$&");
-    }
-
     public static bool IsOperator(this TokenType t) => t.IsMathematicOperator() || t.IsEqualityOperator();
     public static bool IsMathematicOperator(this TokenType t) => t is TokenType.Plus or TokenType.Minus or TokenType.Multiply or TokenType.Divide;
     public static bool IsEqualityOperator(this TokenType t) => t is TokenType.DoubleEquals or TokenType.GreaterThan or TokenType.LessThan;
@@ -51,4 +46,6 @@ public static class Utils{
 
         return output;
     }
+
+    public static int GetNumberOffset(this Type type) => type == typeof(FuncInt) ? 0 : 10_000;
 }
